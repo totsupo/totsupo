9d7b9d7b21d4e63178ffdad0e0aeab59d6234a59
seina.maeda <ayrton19601027@gmail.com>
2025-05-17 15:48:15 +0900

    不要なコンポーネント・ページの削除

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,8 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import NewsPage from './pages/NewsPage'
-import EventsPage from './pages/EventsPage'
-import SpotsPage from './pages/SpotsPage'
-import FoodPage from './pages/FoodPage'
 import ContactPage from './pages/ContactPage'
 import NewsDetailPage from './pages/NewsDetailPage'
-import EventDetailPage from './pages/EventDetailPage'
-import SpotDetailPage from './pages/SpotDetailPage'
-import FoodDetailPage from './pages/FoodDetailPage'
 
 function App() {
   const location = useLocation()
@@ -28,12 +22,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/news" element={<NewsPage />} />
         <Route path="/news/:id" element={<NewsDetailPage />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/events/:id" element={<EventDetailPage />} />
-        <Route path="/spots" element={<SpotsPage />} />
-        <Route path="/spots/:id" element={<SpotDetailPage />} />
-        <Route path="/food" element={<FoodPage />} />
-        <Route path="/food/:id" element={<FoodDetailPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
       <Footer />
